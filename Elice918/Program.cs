bcac8ac29150375e94a1d7412a1cb6f370120fde
Elif_918 <onlyyamada4ever@gmail.com>
2026-02-16 03:53:58 +0400

    Added Ala

--- Elice918/Program.cs
+++ Elice918/Program.cs
@@ -57,7 +57,7 @@ namespace EliceBot
             }
             if (command == "도움")
             {
-                await message.Channel.SendMessageAsync("도움이 필요하신가요?\n❓현재 사용 가능한 명령어는 test, 제피르, 엘리스, 엘리프가 있어요!\n📩기타 내용이나 문의는 주인님에게 문의해주세요!");
+                await message.Channel.SendMessageAsync("도움이 필요하신가요?\n❓현재 사용 가능한 명령어는 test, 제피르, 엘리스, 엘리프,Ala(ala)가 있어요!\n📩기타 내용이나 문의는 주인님에게 문의해주세요!");
             }
             if (command == "제피르")
             {
@@ -70,6 +70,10 @@ namespace EliceBot
             if (command == "엘리프")
             {
                 await message.Channel.SendMessageAsync("ご主人様..♡");
+            }
+            if (command == "Ala" || command == "ala")
+            {
+                await message.Channel.SendMessageAsync("My best polish friend met in roblox 6 or 5 idk months);
             }
         }
 
